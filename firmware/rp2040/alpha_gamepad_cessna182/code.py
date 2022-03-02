1b06dc8cfc1612c2d967336f350b57c6f45f3f26
Drew Coker <drew.coker@gmail.com>
2022-03-02 10:21:33 -0600

    Updated report, moving to generic configuration settings

--- firmware/rp2040/alpha_gamepad_cessna182/code.py
+++ firmware/rp2040/alpha_gamepad_cessna182/code.py
@@ -25,7 +25,14 @@ from adafruit_hid import find_device
 def range_map(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
-print("SETTING UP CONFIG")
+
+#######################
+# Power up configuration
+#######################
+
+print("--------------------")
+print("Power up configuration")
+print("--------------------")
 
 
 #######################
@@ -64,23 +71,30 @@ gpi_string = [] #for power up report
 gpi_ind = 0
 
 gpi.append(digitalio.DigitalInOut(board.GP0))
-gpi_string.append('PICO GPI 0')
+gpi_string.append('PICO GPI 0: PLACEHOLDER SPST TOGGLE')
 gpi[gpi_ind].direction = digitalio.Direction.INPUT
 gpi[gpi_ind].pull = digitalio.Pull.UP
 gpi_ind = gpi_ind + 1
-
 gpi.append(digitalio.DigitalInOut(board.GP1))
-gpi[1].direction = digitalio.Direction.INPUT
-gpi[1].pull = digitalio.Pull.UP
+gpi_string.append('PICO GPI 1: PLACEHOLDER')
+gpi[gpi_ind].direction = digitalio.Direction.INPUT
+gpi[gpi_ind].pull = digitalio.Pull.UP
+gpi_ind = gpi_ind + 1
 gpi.append(digitalio.DigitalInOut(board.GP2))
-gpi[2].direction = digitalio.Direction.INPUT
-gpi[2].pull = digitalio.Pull.UP
+gpi_string.append('PICO GPI 2: PLACEHOLDER')
+gpi[gpi_ind].direction = digitalio.Direction.INPUT
+gpi[gpi_ind].pull = digitalio.Pull.UP
+gpi_ind = gpi_ind + 1
 gpi.append(digitalio.DigitalInOut(board.GP3))
-gpi[3].direction = digitalio.Direction.INPUT
-gpi[3].pull = digitalio.Pull.UP
+gpi_string.append('PICO GPI 3: PLACEHOLDER')
+gpi[gpi_ind].direction = digitalio.Direction.INPUT
+gpi[gpi_ind].pull = digitalio.Pull.UP
+gpi_ind = gpi_ind + 1
 gpi.append(digitalio.DigitalInOut(board.GP22)) #onboard pushbutton
-gpi[4].direction = digitalio.Direction.INPUT
-gpi[4].pull = digitalio.Pull.UP
+gpi_string.append('PICO GPI 22: ONBOARD PUSHBUTTON')
+gpi[gpi_ind].direction = digitalio.Direction.INPUT
+gpi[gpi_ind].pull = digitalio.Pull.UP
+gpi_ind = gpi_ind + 1
 
 # On board ADCs
 NUM_ANALOG = 1
@@ -88,18 +102,33 @@ analog_values = []
 adc = []
 adc_string = []
 adc.append(analogio.AnalogIn(board.A0)) #0
-adc_string.append('PICO A0')
+adc_string.append('PICO A0: PLACEHOLDER')
 adc.append(analogio.AnalogIn(board.A1)) #1
+adc_string.append('PICO A1: PLACEHOLDER')
 adc.append(analogio.AnalogIn(board.A2)) #2
+adc_string.append('PICO A2: PLACEHOLDER')
 #adc = analogio.AnalogIn(board.A0)
 
 # Debug
 counter = 0
 counter_str = str(counter)
 
-print("COMPLETE")
+print("--------------------")
+print("Hardware Report")
+print("--------------------")
+print("GPIO Mapping:")
+for ii in range(len(gpi)):
+    print("\t" + gpi_string[ii])
 
+print("ADC Mapping:")
+for ii in range(len(adc)):
+    print("\t" + adc_string[ii])
 time.sleep(1)
+
+
+print("--------------------")
+print("Entering HID mode")
+print("--------------------")
 
 while True:
     now = time.monotonic()
@@ -119,11 +148,11 @@ while True:
     axis_values.append(range_map(adc[2].value,0,65535,-127,127))    #slider
 
     button_values = []
-    button_values.append(gpi[0].value)   #0
-    button_values.append(gpi[1].value)   #1
-    button_values.append(gpi[2].value)   #2
-    button_values.append(gpi[3].value)   #3
-    button_values.append(gpi[4].value)   #4
+    button_values.append(not gpi[0].value)   #0
+    button_values.append(not gpi[1].value)   #1
+    button_values.append(not gpi[2].value)   #2
+    button_values.append(not gpi[3].value)   #3
+    button_values.append(not gpi[4].value)   #4
     button_values.append(False)          #5
     button_values.append(False)          #6
     button_values.append(False)          #7
